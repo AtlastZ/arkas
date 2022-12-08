a05c3724896a52b9f0bca11b41e72af4ba34abc5
AtlastZ <neolight413666@gmail.com>
2022-12-08 22:26:16 +0700

    main

--- index.js
+++ index.js
@@ -192,6 +192,8 @@ function handleEvent(event) {
       "type": "sticker",
       "packageId": "11539",
       "stickerId": "52114129",
+      type: "text",
+      text: "อะไรนะคะ? \nลองพิมพ์ 'คำสั่ง'\nIdk,try to typing 'command'",
     }
     return client.replyMessage(event.replyToken, payload);
   }
